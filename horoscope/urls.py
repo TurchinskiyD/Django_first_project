675b76e31e24b96ed24eaf695d4153d0a2af89ae
DIMA\turch <turchinskiyd@gmail.com>
2023-06-27 14:05:03 +0300

    Динамічний URL

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import aries, taurus, gemini, cancer, leo, virgo, libra, scorpion, sagittarius, capricorn, aquarius, pisces
-
+from .views import get_info_about_zodiac
 
 urlpatterns = [
-    path('aries/', aries),
-    path('taurus/', taurus),
-    path('gemini/', gemini),
-    path('cancer/', cancer),
-    path('leo/', leo),
-    path('virgo/', virgo),
-    path('libra/', libra),
-    path('scorpion/', scorpion),
-    path('sagittarius/', sagittarius),
-    path('capricorn/', capricorn),
-    path('aquarius/', aquarius),
-    path('pisces/', pisces)
+    path('<sign_zodiac>/', get_info_about_zodiac)
+    # path('aries/', aries),
+    # path('taurus/', taurus),
+    # path('gemini/', gemini),
+    # path('cancer/', cancer),
+    # path('leo/', leo),
+    # path('virgo/', virgo),
+    # path('libra/', libra),
+    # path('scorpion/', scorpion),
+    # path('sagittarius/', sagittarius),
+    # path('capricorn/', capricorn),
+    # path('aquarius/', aquarius),
+    # path('pisces/', pisces)
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,51 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import HttpResponse, HttpResponseNotFound
 
 
-# Create your views here.
-def aries(request):
-    return HttpResponse("Знак зодіаку Овен. March 21 — April 20")
-
-
-def taurus(request):
-    return HttpResponse("Знак зодіаку Телець. April 21 — May 22")
-
-
-def gemini(request):
-    return HttpResponse("Знак зодіаку Близнюки. May 23 — June 21")
-
-
-def cancer(request):
-    return HttpResponse("Знак зодіаку Рак. June 22 — July 22")
-
-
-def leo(request):
-    return HttpResponse("Знак зодіаку Лев. July 23 — August 22")
-
-
-def virgo(request):
-    return HttpResponse("Знак зодіаку Діва. August 23 — September 22")
-
-
-def libra(request):
-    return HttpResponse("Знак зодіаку Терези. September 23 — October 22")
-
-
-def scorpion(request):
-    return HttpResponse("Знак зодіаку Скорпіон. October 23 — November 21")
-
-
-def sagittarius(request):
-    return HttpResponse("Знак зодіаку Стрілець. November 22 — December 22")
-
-
-def capricorn(request):
-    return HttpResponse("Знак зодіаку Козоріг. December 23 — January 20")
-
-
-def aquarius(request):
-    return HttpResponse("Знак зодіаку Водолій. January 21 — February 19")
-
-
-def pisces(request):
-    return HttpResponse("Знак зодіаку Риби. February 20 — March 20")
+def get_info_about_zodiac(request, sign_zodiac):
+    if sign_zodiac == "aries":
+        return HttpResponse("Знак зодіаку Овен. March 21 — April 20. Овна потрібно обіймати, коли він не посміхається, цілувати, коли тупить, і годувати, коли в нього істерика.")
+    elif sign_zodiac == "taurus":
+        return HttpResponse("Знак зодіаку Телець. April 21 — May 22. Щастя для Тельця — це багато подорожувати, смачно їсти і бути з коханою людиною")
+    elif sign_zodiac == "gemini":
+        return HttpResponse("Знак зодіаку Близнюки. May 23 — June 21. Якщо Близнюки вирішать зробити когось щасливим, то людину вже ніщо не врятує!")
+    elif sign_zodiac == "cancer":
+        return HttpResponse("Знак зодіаку Рак. June 22 — July 22. Коли терпінню Раків настає межа, тоді починається повне беззаконня.")
+    else:
+        return HttpResponseNotFound(f"Невідомий знак зодіаку - {sign_zodiac}")
+#
+#
+# def leo(request):
+#     return HttpResponse("Знак зодіаку Лев. July 23 — August 22")
+#
+#
+# def virgo(request):
+#     return HttpResponse("Знак зодіаку Діва. August 23 — September 22")
+#
+#
+# def libra(request):
+#     return HttpResponse("Знак зодіаку Терези. September 23 — October 22")
+#
+#
+# def scorpion(request):
+#     return HttpResponse("Знак зодіаку Скорпіон. October 23 — November 21")
+#
+#
+# def sagittarius(request):
+#     return HttpResponse("Знак зодіаку Стрілець. November 22 — December 22")
+#
+#
+# def capricorn(request):
+#     return HttpResponse("Знак зодіаку Козоріг. December 23 — January 20")
+#
+#
+# def aquarius(request):
+#     return HttpResponse("Знак зодіаку Водолій. January 21 — February 19")
+#
+#
+# def pisces(request):
+#     return HttpResponse("Знак зодіаку Риби. February 20 — March 20")
